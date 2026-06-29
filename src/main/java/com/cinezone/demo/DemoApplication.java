d6420d4a3445ec55b78965c0180e30a88780a1f3
Emerson-Lezama-Vera <emerson_00_35@hotmail.com>
2026-06-29 17:59:46 -0500

    fix(backend): configurar timezone a America/Lima para corregir desfase en listado de funciones publicas

--- src/main/java/com/cinezone/demo/DemoApplication.java
+++ src/main/java/com/cinezone/demo/DemoApplication.java
@@ -3,10 +3,18 @@ package com.cinezone.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
