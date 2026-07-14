2c50cfd0fb8cf746794333618054f529d4fd59bf
Emerson-Lezama-Vera <emerson_00_35@hotmail.com>
2026-07-14 15:23:15 -0500

    fix(api): anadir prefijo v1 a AdminSedeController para unificar rutas

--- src/main/java/com/cinezone/demo/api/AdminSedeController.java
+++ src/main/java/com/cinezone/demo/api/AdminSedeController.java
@@ -7,7 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin/sedes")
+@RequestMapping("/api/v1/admin/sedes")
 @RequiredArgsConstructor
 public class AdminSedeController {
 
